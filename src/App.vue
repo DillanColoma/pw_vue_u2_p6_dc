4e9515bf6788858bd50672d1fbb6a8fc4f79a5aa
Dillan Coloma <dillanrenatocolomapozo@gmail.com>
2024-06-07 13:12:08 -0500

    Taller 17 Propiedades Computadas

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <template>
-<Contador></Contador>
-</template>
 
-<script>
-import Contador from './components/Contador.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Contador
+  <img src="./assets/logo.png" alt="NONE">
+  <Contador titulo="Estudiante"/>   <!---[3]-->
+  <Contador titulo="Profesor"/>
+  <Contador />
+  
+  
+  
+  </template>
+  
+  <script>
+  
+  import Contador from "./components/Contador.vue" /*[1]*/ 
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      Contador  /*[2]*/ 
+  
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
+  
+  
+  
+  
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,11 +1,87 @@
 <template>
-    <h2>Contador</h2>
-    <p>100</p>
+  <h2>{{ presentar }}</h2>
+  <!--<p>{{ numero }} <sup>2</sup>={{ calcularCuadrado() }}</p>  LLAMADA A UN METODO-->
+  <p>{{ numero }}<sup>2</sup>={{ calcular }}</p>
+
+  <div>
+    <button v-on:click="incrementar">+1</button>
+    <button v-on:click="disminuir">-1</button>
+  </div>
   </template>
   
   <script>
-  export default {};
+  /*import Contador from './components/Contador.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      Contador
+    }
+  }*/
+  export default{
+  
+    props:["titulo","propN"],
+    data(){
+      return{
+        /*propiedades reactivas*/
+        numero:5,
+      };
+    },
+    methods:{
+      calcularCuadrado(){
+        console.log("Ingreso al método");
+        return(
+          this.numero*this.numero
+        );/*Usar siempre el this, en html no es necesario*/
+      },
+      calcularCuadrado2(num1, num2){
+        console.log("Ingreso al método 2");
+        return num1*num2
+      },
+      incrementar(){
+        this.numero ++;
+        /*this.calcular() Esto no es necesrio ya que lo nceseario es
+        lo que declara la propiedad computada*/
+      },
+      disminuir(){
+        this.numero --;
+      },
+  
+    },
+    computed:{
+      calcular(){
+        console.log("Ingreso a la propiedad computada");
+        return this.numero *this.numero;
+      },
+      presentar(){
+        if(this.titulo!==undefined){
+          return this.titulo;
+        }else{
+          return"Texto que quiero";
+  
+        }
+      }
+    },
+  
+  };
   </script>
   
   <style>
+  * {
+    background: rgb(255, 246, 80);
+  }
+  button {
+    background: rgb(155, 208, 9);
+    border-radius: 5px;
+    border: 1px solid white;
+    color: rgb(255, 255, 255);
+    margin: 5px 5px;
+    padding: 5px 20px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background: #5aa67b;
+  }
   </style>
+  
